2df3d691ba5bbf43cc35e7c1794f78e0d2c0e420
wangyongshan <wangys0927@gmail.com>
2026-01-24 21:59:39 +0800

    更新代码

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -7,7 +7,27 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+      "@": path.resolve(__dirname, "./src")
+    }
   },
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/js/[name]-[hash].js",
+        chunkFileNames: "assets/js/[name]-[hash].js",
+        assetFileNames(assetInfo) {
+          const name = assetInfo.name;
+          if (name.endsWith(".css")) {
+            return "assets/css/[name]-[hash].css";
+          }
+          const imgExts = [".png", ".jpg", ".jpeg", ".gif", ".svg", ".webp"];
+          if (imgExts.some((ext) => name.endsWith(ext))) {
+            return "assets/img/[name]-[hash].[extname]";
+          }
+          return "assets/[name]-[hash].[extname]";
+        }
+      }
+    }
+  }
 })
